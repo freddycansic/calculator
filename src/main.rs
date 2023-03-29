3ac36040935fa10a35d97a8951e0fe79c2dc5b27
Freddy Cansick <freddycansick@gmail.com>
2023-03-29 21:17:01 +0100

    Shunting-yard

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,91 @@
-#![feature(is_some_and)]
+#![feature(is_some_and, let_chains)]
 
-use std::{collections::VecDeque, fmt::Debug, io};
+use std::{collections::VecDeque, fmt::Debug, io, mem::discriminant};
 
 use derive_more::Constructor;
 use ptree::{print_tree, TreeBuilder};
 use r3bl_rs_utils::Arena;
 
-#[derive(Debug, Clone, Default, PartialEq)]
-enum TokenType {
+#[derive(Debug, Clone, PartialEq)]
+enum Token {
     OpeningBracket,
     ClosingBracket,
-    Operator,
-    #[default]
-    Number,
+    Operator(Operator),
+    Number(f32),
 }
 
-#[derive(Debug, Clone, Default, Constructor)]
-struct Token {
-    token_type: TokenType,
-    token: String,
+impl Default for Token {
+    fn default() -> Self {
+        Token::Number(0.0)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
+enum Associativity {
+    #[default]
+    Left,
+    Right,
+}
+
+#[derive(Constructor, Clone, Debug, PartialEq)]
+struct Operator {
+    symbol: char,
+    associativity: Associativity,
+    precedence: u32,
 }
 
 fn main() {
-    // let mut infix = String::new();
-    // println!("Enter infix statement: ");
-    // std::io::stdin().read_line(&mut infix).unwrap();
+    let mut infix = String::new();
+    println!("Enter infix statement: ");
+    std::io::stdin().read_line(&mut infix).unwrap();
 
-    let operators = ['+', '-', '*', '/', '^'];
-    let infix = "(((1+2)*4)/3)";
-    println!("{}", infix);
+    let operators = [
+        Operator::new('+', Associativity::Left, 0),
+        Operator::new('-', Associativity::Left, 0),
+        Operator::new('*', Associativity::Left, 1),
+        Operator::new('/', Associativity::Left, 1),
+        Operator::new('%', Associativity::Left, 1),
+        Operator::new('^', Associativity::Right, 2),
+    ];
 
-    let tokens = tokenise(infix, &operators);
-
-    // if let Err(err) = print_arena_tree(&tree) {
-    //     panic!("{}", err);
-    // }
-
-    let parse_tree = build_parse_tree(&tokens);
-    let postfix_traveral = postfix_traversal(&parse_tree);
-
-    println!("{}", evaluate(&parse_tree, &postfix_traveral));
+    let tokens = tokenise(&infix, &operators);
+    let rpn = shunting_yard(&tokens);
+    println!("{}", evaluate_rpn(&rpn));
 }
 
-fn build_parse_tree(tokens: &Vec<Token>) -> Arena<Token> {
-    let mut tree = Arena::<Token>::new();
-    let mut current = 0;
-    tree.add_new_node(Token::default(), None);
-
-    for token in tokens.iter() {
-        match token.token_type {
-            TokenType::OpeningBracket => {
-                current = tree.add_new_node(Token::default(), Some(current));
-            }
-            TokenType::ClosingBracket => {
-                current = create_parent_if_none(&mut tree, current);
-            }
-            TokenType::Operator => {
-                tree.get_node_arc(current).unwrap().write().unwrap().payload = token.clone();
-                current = tree.add_new_node(Token::default(), Some(current));
-            }
-            TokenType::Number => {
-                tree.get_node_arc(current).unwrap().write().unwrap().payload = token.clone();
-                current = create_parent_if_none(&mut tree, current);
-            }
-        }
-    }
-
-    tree
-
-}
-
-fn tokenise(infix: &str, operators: &[char]) -> Vec<Token> {
+fn tokenise(infix: &str, operators: &[Operator]) -> Vec<Token> {
     let infix = infix.split_whitespace().collect::<Vec<&str>>().join("");
 
     let mut tokens: Vec<Token> = Vec::new();
-    let mut infix_chars = infix.char_indices().peekable();
+    let mut infix_chars = infix.char_indices();
 
     while let Some((index, char)) = infix_chars.next() {
-        if operators.contains(&char) {
-            tokens.push(Token::new(TokenType::Operator, char.to_string()))
+        if let Some(op) = operators
+            .iter()
+            .find_map(|op| (op.symbol == char).then(|| op))
+        {
+            tokens.push(Token::Operator(op.clone()));
         } else if char == '(' {
-            tokens.push(Token::new(TokenType::OpeningBracket, char.to_string()))
+            tokens.push(Token::OpeningBracket)
         } else if char == ')' {
-            tokens.push(Token::new(TokenType::ClosingBracket, char.to_string()))
+            tokens.push(Token::ClosingBracket)
         } else if char.is_numeric() {
-            while let Some((index_2, next_char)) = infix_chars.clone().peek() {
-                if next_char.is_numeric() {
-                    infix_chars.next();
-                } else {
-                    tokens.push(Token::new(
-                        TokenType::Number,
-                        infix[index..*index_2].to_string(),
-                    ));
-                    break;
-                }
+            let next_non_num_index = match infix
+                .chars()
+                .skip(index)
+                .position(|char| !char.is_numeric())
+            {
+                Some(next_non_num_index) => next_non_num_index + index,
+                None => infix.len(), // if there arent any more chars then end the num
+            };
+
+            for _ in 0..next_non_num_index - index - 1 {
+                infix_chars.next();
             }
+
+            tokens.push(Token::Number(
+                infix[index..next_non_num_index].parse::<f32>().unwrap(),
+            ));
         }
     }
 
@@ -148,34 +139,25 @@ where
     }
 }
 
-
-fn evaluate(tree: &Arena<Token>, path: &VecDeque<usize>) -> f32 {
+fn evaluate_rpn(rpn_tokens: &Vec<Token>) -> f32 {
     let mut stack = Vec::<f32>::new();
-    for node_id in path.iter() {
-
-        let token = tree
-            .get_node_arc(*node_id)
-            .unwrap()
-            .read()
-            .unwrap()
-            .payload
-            .clone();
-
-        match token.token_type {
-            TokenType::Operator => {
+    for token in rpn_tokens.iter() {
+        match token {
+            Token::Operator(operator) => {
                 let operand_1 = stack.pop().unwrap();
                 let operand_2 = stack.pop().unwrap();
 
-                match token.token.as_str() {
-                    "+" => stack.push(operand_2 + operand_1),
-                    "-" => stack.push(operand_2 - operand_1),
-                    "*" => stack.push(operand_2 * operand_1),
-                    "/" => stack.push(operand_2 / operand_1),
-                    "^" => stack.push(operand_2.powf(operand_1)),
+                match operator.symbol {
+                    '+' => stack.push(operand_2 + operand_1),
+                    '-' => stack.push(operand_2 - operand_1),
+                    '*' => stack.push(operand_2 * operand_1),
+                    '/' => stack.push(operand_2 / operand_1),
+                    '^' => stack.push(operand_1.powf(operand_2)),
+                    '%' => stack.push(operand_2 % operand_1),
                     _ => panic!(),
                 }
             }
-            TokenType::Number => stack.push(token.token.parse::<f32>().unwrap()),
+            Token::Number(number) => stack.push(*number),
             _ => panic!(),
         }
     }
@@ -202,7 +184,7 @@ where
             build_string_tree(builder, tree, child_id);
         }
     }
-    
+
     builder.end_child();
 }
 
@@ -216,4 +198,62 @@ where
     print_tree(&output)?;
 
     Ok(())
+}
+
+// For all the input tokens [S1]:
+//  Read the next token [S2];
+//  If token is an operator (x) [S3]:
+//      While there is an operator (y) at the top of the operators stack and either (x) is left-associative and its precedence is less or equal to that of (y), or (x) is right-associative and its precedence is less than (y) [S4]:
+//          Pop (y) from the stack [S5];
+//          Add (y) output buffer [S6];
+//      Push (x) on the stack [S7];
+//  Else If token is left parenthesis, then push it on the stack [S8];
+//  Else If token is a right parenthesis [S9]:
+//      Until the top token (from the stack) is left parenthesis, pop from the stack to the output buffer [S10];
+//      Also pop the left parenthesis but don't include it in the output buffer [S11];
+//  Else add token to output buffer [S12].
+//  While there are still operator tokens in the stack, pop them to output [S13]
+
+fn shunting_yard(tokens: &Vec<Token>) -> Vec<Token> {
+    let mut rpn_tokens = Vec::<Token>::new();
+    let mut stack = VecDeque::<Token>::new();
+
+    for token in tokens.iter() {
+        match token {
+            Token::Operator(operator_x) => {
+                // if the top of stack is an operator type. discriminant means disregard the contents of the enum
+                while let Some(Token::Operator(operator_y)) = stack.back() {
+                    if discriminant(stack.back().unwrap()) == discriminant(token)
+                        && ((operator_x.associativity == Associativity::Left
+                            && operator_x.precedence <= operator_y.precedence)
+                            || (operator_x.associativity == Associativity::Right
+                                && operator_x.precedence < operator_y.precedence))
+                    {
+                        rpn_tokens.push(stack.pop_back().unwrap());
+                    } else {
+                        break;
+                    }
+                }
+
+                stack.push_back(token.clone());
+            }
+            Token::OpeningBracket => stack.push_back(token.clone()),
+            Token::ClosingBracket => {
+                while let Some(token) = stack.pop_back() {
+                    if token != Token::OpeningBracket {
+                        rpn_tokens.push(token.clone())
+                    } else {
+                        break;
+                    }
+                }
+            }
+            Token::Number(_) => rpn_tokens.push(token.clone()),
+        }
+    }
+
+    while let Some(token) = stack.pop_back() {
+        rpn_tokens.push(token.clone())
+    }
+
+    rpn_tokens
 }
